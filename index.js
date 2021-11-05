0c9a5febc4ae789e3a11219d769ad82898f7697d
WILLIAM DE SA RODRIGUES <wsr2k2@gmail.com>
2021-11-05 10:33:19 -0300

    Criação das rotas, integração com mongoose

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 const express = require("express");
-
 const app = express();
+
 app.use(express.json());
+
+const Conn = require("./model/conn/index"); //importando a conexao
+
+Conn(); //executa a func de conexao
 
 const port = 3000;
 
+const estadosRouter = require("./routers/estados.routes");
+app.use('/estados',estadosRouter);
 
-app.get("/", (req,res) => {
-  res.status(200).json({message:"Pagina inicial"});
-});
+const cidadesRouter = require("./routers/cidades.routes");
+app.use('/cidades',cidadesRouter);
 
-const paisesRouter = require("./paises");
-app.use("/paises", paisesRouter);
-
-const estadosRouter = require("./estados");
-app.use("/estados", estadosRouter);
-
-const cidadesRouter = require("./cidades");
-app.use("/cidades", cidadesRouter);
 
 app.listen(port, () => {
   console.info(`App rodando em: http://localhost:${port}`);
